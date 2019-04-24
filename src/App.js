fde4397e346ae6b1e23d8b1d23e624cb2e87d232
Anthony DiRusso <ossurid@gmail.com>
2019-04-24 11:03:25 -0400

    adds map for an array

--- src/App.js
+++ src/App.js
@@ -9,28 +9,43 @@ class App extends React.Component {
     this.state= {
       inputText: ''
       };
+
   }
 
   render() {
+
+    const names = ['Oakley', 'Milla', 'Angela'];
+
+
     return (
       <div className="App">
         <header className="App-header">
-          <input 
-            onChange={ (e) => {
-              console.log(e.target.value);
-              this.setState({
-                inputText: e.target.value
-              });
-            } }
+        <ul>
+          {names.map(name => <li>{name}</li>)} {/* arrow function Implicitly Returning*/}
+        </ul>
+          
+          {/* <input 
+            onChange={ this._updateInputText }
           />
           <Output 
             text={this.state.inputText}
-          />
+          /> */}
         </header>
-      </div>
+      </div> 
     );
   }
   
+  _updateInputText = (e) => {
+    console.log(e.target.value);
+    // this is a request for a change 
+    // that will take place in the future
+    this.setState({
+      inputText: e.target.value
+    }, () => {
+      console.log(`State is now ${this.state.inputText}`)
+    });
+    
+  } 
 }
 
 export default App;
