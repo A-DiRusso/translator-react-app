fefac7eb989b34b3f9012f8b2fd71db113921159
Anthony DiRusso <ossurid@gmail.com>
2019-04-24 10:06:08 -0400

    converts App from function to class

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import React from 'react';
 import './App.css';
 import Output from './Output';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <input 
-          onChange={ (e) => {
-            console.log(e.target.value);
-          } }
-        />
-        <Output 
-          text="does it work?"
-        />
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input 
+            onChange={ (e) => {
+              console.log(e.target.value);
+            } }
+          />
+          <Output 
+            text="does it work?"
+          />
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
